34212b22401f13e0e23e03008a68866a9cb85979
acl-dev <acl@local>
2026-09-04 00:58:44 +0900

    chore: rebuild dist binaries for 0.5.0

--- cmd/aclagent/main.go
+++ cmd/aclagent/main.go
@@ -18,7 +18,7 @@ import (
 	"github.com/githubflyideas/acl-edit-only/internal/h3c/plan"
 )
 
-const agentVersion = "0.4.1"
+const agentVersion = "0.5.0"
 
 func main() {
 	if err := run(); err != nil {
